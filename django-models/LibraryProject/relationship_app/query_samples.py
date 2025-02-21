3f63a83f4fd24c6a978ae407a40f6937ab9cd644
Ahmed Hassan <drahmedhassan.1990@gmail.com>
2025-02-21 22:33:26 +0200

    11 week 5

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -3,12 +3,12 @@ from . import models
 
 book_title = "b"
 b= models.Book.objects.get(title=book_title)
-b_by_author = b.author_set.all()
+b_by_author = b.author.all()
 
 library_name = "a"
 l = models.Library.objects.get(name=library_name)
-b_in_lib = l.books_set.all()
+b_in_lib = l.books.all()
 
 librarian_name = "s"
 lib = models.Librarian.objects.get(name=librarian_name)
-libn_for_lib = lib.library_set.all()
+libn_for_lib = lib.library.all()
