5409906e66136fc2331b48e116b61971e055596c
Ahmed Hassan <drahmedhassan.1990@gmail.com>
2025-02-22 15:45:50 +0200

    11 week T2 3

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,7 +3,8 @@ from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-from django.contrib.auth.views import login, LoginView, LogoutView
+from django.contrib.auth.views import  LoginView, LogoutView
+from django.contrib.auth import login
 from .models import Library, Book
 
 # Create your views here.
