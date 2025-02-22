6bd70403c9c8e777b1708796b98edd7a5991e15c
Ahmed Hassan <drahmedhassan.1990@gmail.com>
2025-02-22 15:37:39 +0200

    11 week T2 1

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,28 @@
+"""
+URL configuration for LibraryProject project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.urls import path
-from .views import list_books, LibraryDetailView
+from .views import list_books, LibraryDetailView, SignUpView
+from django.contrib.auth.views import LoginView, LogoutView
+
 
 urlpatterns = [
     path('booklist/', list_books, name='booklist'),
-    path('library/', LibraryDetailView.as_view(), name='library')
+    path('library/', LibraryDetailView.as_view(), name='library'),
+    path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
+    path("signup/", SignUpView.as_view(template_name='registration/register.html'), name="signup"),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 from .models import Library, Book
 
 # Create your views here.
@@ -14,3 +17,11 @@ class LibraryDetailView(DetailView):
     template_name = "relationship_app/library_detail.html"
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        return context
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/register.html'
+
+
