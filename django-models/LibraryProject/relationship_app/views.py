c16b7a7fbc775f03c4143ecf61df0cb5ccdf3e85
Ahmed Hassan <drahmedhassan.1990@gmail.com>
2025-02-22 21:01:45 +0200

    11 week T3 2

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -67,14 +67,14 @@ def is_member(user):
     return user.is_authenticated and getattr(user, 'userprofile', None) and user.userprofile.role == 'Member' 
 
 @user_passes_test(is_admin)
-def admin_view(request):
+def admin(request):
     return render(request, 'relationship_app/admin_view.html', {'role': 'Admin'})
 
 @user_passes_test(is_librarian)
-def librarian_view(request):
+def librarian(request):
     return render(request, 'relationship_app/librarian_view.html', {'role': 'Librarian'})
 
 @user_passes_test(is_member)
-def member_view(request):
+def member(request):
     return render(request, 'relationship_app/member_view.html', {'role': 'Member'})
 
