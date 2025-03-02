d0e6f04bc2bb3b147a23c61a23ee7ce6f2546c1f
Ahmed Hassan <drahmedhassan.1990@gmail.com>
2025-03-02 20:58:50 +0200

    week 11 2 2

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
 from .models import Book
+from .forms import ExampleForm
 # Create your views here.
 
 
@@ -8,3 +9,16 @@ from .models import Book
 def edit_view(request):
     context = { "book_list" : Book.objects.aLL() }
     return render(request, "bookshelf/edit.html", context)
+
+def example_view(request):
+    if request.method == "POST":
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data["name"]
+            email = form.cleaned_data["email"]
+            message = form.cleaned_data["message"]
+            return render(request, "bookshelf/success.html", {"name": name})
+    else:
+        form = ExampleForm()
+
+    return render(request, "bookshelf/example_form.html", {"form": form})
