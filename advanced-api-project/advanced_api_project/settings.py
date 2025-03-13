1b341f6922d9abd6a291543821f315c6a93c38fa
Ahmed Hassan <drahmedhassan.1990@gmail.com>
2025-03-13 04:28:49 +0200

    week 13 02 01

--- advanced-api-project/advanced_api_project/settings.py
+++ advanced-api-project/advanced_api_project/settings.py
@@ -126,6 +126,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',  # Require authentication for all views
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',  
     ],
 }
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,15 +1,29 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, filters
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
+import django_filters
  
 # Create your views here.
+
+class BookFilter(django_filters.Filterset):
+    title = django_filters.Charfilter(lookup_expr='icontains')
+    author = django_filters.Charfilter(lookup_expr='icontains')
+    publication_year = django_filters.Numderfilter(lookup_expr='exact')
+    publication_year__gte = django_filters.NumberFilter(field_name='publication_year', lookup_expr='gte')
+    publication_year__lte = django_filters.NumberFilter(field_name='publication_year', lookup_expr='lte')
+
 
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_class = BookFilter
+    search_fields = ['title','author','publication_year']
+    ordering_fields = ['title','author','publication_year']
 
 class BookDetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
