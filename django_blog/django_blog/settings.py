a04e5d746ad306fb782b71b01f694eefb4a79260
Ahmed Hassan <drahmedhassan.1990@gmail.com>
2025-03-17 05:33:54 +0200

    week 14 00 04

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -76,8 +76,15 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': BASE_DIR / 'mydatabase',
+        'USER': 'root',  
+        'PASSWORD': 'mypass',  
+        'HOST': 'localhost',  
+        'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4', 
+        },
     }
 }
 
