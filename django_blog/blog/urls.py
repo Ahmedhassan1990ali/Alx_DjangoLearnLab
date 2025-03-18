e481eacbcdb806bdaf3f7c35dba4a60a42b0ef99
Ahmed Hassan <drahmedhassan.1990@gmail.com>
2025-03-18 07:32:22 +0200

    week 14 01 01

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.contrib.auth.views import LoginView,LogoutView
+from blog.views import register
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('blog/',include('blog.urls'))
+    path("login/",LoginView.as_view(template_name="blog/login.html"),name="login"),
+    path("logout/",LogoutView.as_view(template_name="blog/logout.html"),name="logout"),
+    path("register/",register,name="register"),
 ]
--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -76,15 +76,12 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': BASE_DIR / 'mydatabase',
-        'USER': 'root',  
-        'PASSWORD': 'mypass',  
-        'HOST': 'localhost',  
-        'PORT': '3306',
-        'OPTIONS': {
-            'charset': 'utf8mb4', 
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': '',        
+        'PASSWORD': '',    
+        'HOST': '',        
+        'PORT': '',
     }
 }
 
