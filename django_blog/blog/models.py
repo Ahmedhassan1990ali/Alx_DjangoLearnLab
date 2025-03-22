2330b6eb9a72bee33bcaf36849dcbc96017347b8
Ahmed Hassan <drahmedhassan.1990@gmail.com>
2025-03-22 05:16:08 +0200

    week 14 04 02

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,14 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
+from taggit.models import TaggedItemBase, GenericTaggedItemBase
 
 # Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=25, unique=True)
+    
+    def __str__(self):
+        return self.name
+    
+class CustomTaggedItem(GenericTaggedItemBase, TaggedItemBase): 
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name="taggit_items")
+
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    taggit_tags = TaggableManager(through=CustomTaggedItem, blank=True)
 
     def get_absolute_url(self):
         return reverse('post_detail', kwargs={'pk': self.pk})    
@@ -29,9 +42,5 @@ class Comment(models.Model):
     def __str__(self):
         return self.content[:50]
     
-class Tag(models.Model):
-    name = models.CharField(max_length=25)
-    posts = models.ManyToManyField(Post, related_name="tags")
 
-    def __str__(self):
-        return self.name
+
