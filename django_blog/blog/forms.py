f26a5d4e321e857fcb2426467ee1da4ba7584631
Ahmed Hassan <drahmedhassan.1990@gmail.com>
2025-03-22 06:41:38 +0200

    week 14 04 04

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from blog.models import Post, Comment
+from taggit.forms import TagWidget
 
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
@@ -11,11 +12,14 @@ class PostForm(forms.ModelForm):
     tags = forms.CharField(
         required=False,
         help_text="Enter tags separated by commas",
-        widget=forms.TextInput(attrs={'placeholder': 'e.g. django, python, web'})
+        widget=TagWidget(attrs={'placeholder': 'e.g. django, python, web'})
     )
     class Meta:
         model = Post
         fields = ["title","content"]
+        widgets = {
+            'content': forms.Textarea(attrs={'placeholder': 'Write your post content here...'}),
+        }
     def clean_tags(self):
         tags = self.cleaned_data.get('tags', '')
         return [tag.strip() for tag in tags.split(',') if tag.strip()]
