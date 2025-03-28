74747e1ee0ab6d2ba489a3c2a2c850e4f627d359
Ahmed Hassan <drahmedhassan.1990@gmail.com>
2025-03-28 11:14:10 +0200

    week 15 02 01

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
 from posts.models import Post,Comment
 from posts.serializers import PostSerializer, CommentSerializer
 from posts.permissions import IsOwnerOrReadOnly
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from rest_framework.authentication import TokenAuthentication
 
 # Create your views here.
@@ -13,6 +14,10 @@ class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['title', 'content']
+    search_fields = ['title', 'content']
+
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
@@ -21,6 +26,10 @@ class CommenyViewSet(viewsets.ModelViewSet):
     permission_classes = [IsOwnerOrReadOnly]
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['post', 'author']
+    search_fields = ['content'] 
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'accounts',
-    'posts'
+    'posts',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -134,4 +135,6 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication'
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10, 
 }
